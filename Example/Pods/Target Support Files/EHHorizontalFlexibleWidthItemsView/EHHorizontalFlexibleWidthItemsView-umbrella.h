a3aa907ec0c8ba04f4accd6adac7d8791756d55e
Eric Huang <WaterFlowsEast@gmail.com>
2017-01-10 14:46:53 +0800

    pod install for Example

--- Example/Pods/Target Support Files/EHHorizontalFlexibleWidthItemsView/EHHorizontalFlexibleWidthItemsView-umbrella.h
+++ Example/Pods/Target Support Files/EHHorizontalFlexibleWidthItemsView/EHHorizontalFlexibleWidthItemsView-umbrella.h
@@ -1,5 +1,13 @@
 #import <UIKit/UIKit.h>
 
+#import "EHFlexibleWidthItemsSequentialSelectionView.h"
+#import "EHFlexibleWidthItemsSingleAnimatedSelectionSeparatorView.h"
+#import "EHFlexibleWidthItemsSingleAnimatedSelectionView.h"
+#import "EHHorizontalFlexibleWidthItemsSelectionView.h"
+#import "EHHorizontalFlexibleWidthItemsSeparatorView.h"
+#import "EHHorizontalFlexibleWidthItemsView.h"
+#import "EHHorizontalFlexibleWidthLayout+SeparatorPosition.h"
+#import "EHHorizontalFlexibleWidthLayout.h"
 
 FOUNDATION_EXPORT double EHHorizontalFlexibleWidthItemsViewVersionNumber;
 FOUNDATION_EXPORT const unsigned char EHHorizontalFlexibleWidthItemsViewVersionString[];
